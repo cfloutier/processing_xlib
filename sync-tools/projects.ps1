10bbdfa578d347801005bebf7d56f3d8d7c9b4db
TOFITOP\cflou <cfloutier@gmail.com>
2026-05-26 14:50:32 +0200

    ajout contours

--- sync-tools/projects.ps1
+++ sync-tools/projects.ps1
@@ -3,7 +3,7 @@
 
 # Liste des noms de projets (ordre pour le menu interactif)
 # Les chemins se construisent automatiquement à partir de ces noms
-$projectNames = @("spiral", "perlin_mountains", "image_lines", "gravity", "image_dots")
+$projectNames = @("spiral", "perlin_mountains", "image_lines", "gravity", "image_dots", "image_contours")
 
 # Construire les chemins automatiquement depuis les noms
 $projectPaths = @{}
